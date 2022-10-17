e831a0b861d66fb215f5e851322a645f05b4d3f0
MargaritaWang <112512721+Margaritajellyfish@users.noreply.github.com>
2022-10-17 03:14:03 +0000

    7lab10

--- main.py
+++ main.py
@@ -3,14 +3,16 @@
 # ******************************
 
 
+
 numbers = list(map(int, input().split()))
 
 for i in range(0, len(numbers)):
     min_=99999
     min_index = -1
     for j in range(i, len(numbers)):
-        min_ = numbers[j]
-        min_index = j
+        if numbers[j] < min_:
+         min_ = numbers[j]
+         min_index = j
     temp = numbers[i]
     numbers[i] = min_
     numbers[min_index] = temp
